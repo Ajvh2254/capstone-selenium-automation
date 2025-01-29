7fdbc9b3ea798be993c691ecfe51ff83dbab93de
anthonyhenderson <cuzixsin@gmail.com>
2025-01-28 22:56:56 -0800

    initializeDriver method in WebDriverfactory

--- src/main/java/capstonefactory/driver/WebDriverFactory.java
+++ src/main/java/capstonefactory/driver/WebDriverFactory.java
@@ -3,15 +3,25 @@ package capstonefactory.driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
 public class WebDriverFactory {
 
-    public static void main(String[] args) {
-        initializeDriver();
-    }
+    public WebDriver driver;
 
-    public static void initializeDriver() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://letcode.in/test");
+    public WebDriver initializeDriver() throws IOException {
+        Properties prop = new Properties();
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "//src//test//resources//GlobalData.properties");
+        prop.load(fis);
+        String browserName = prop.getProperty("browser");
+        if (browserName.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        return driver;
     }
 
 }
