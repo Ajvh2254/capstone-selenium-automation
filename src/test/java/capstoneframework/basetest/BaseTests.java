92941c30b00b27644ce0e113151aff5efa6312cb
anthonyhenderson <cuzixsin@gmail.com>
2025-01-29 18:33:11 -0800

    added BaseTests

---
+++ src/test/java/capstoneframework/basetest/BaseTests.java
@@ -0,0 +1,23 @@
+package capstoneframework.basetest;
+
+import capstonefactory.base.BasePage;
+import capstonefactory.driver.WebDriverFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
+
+public class BaseTests extends WebDriverFactory {
+
+    @BeforeMethod
+    public void launchApplication() throws IOException {
+        driver = initializeDriver();
+        BasePage.setDriver(driver);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        quitDriver();
+    }
+
+}
