a5bc33433483329d46b452f7719e3f8c64d834b7
anthonyhenderson <cuzixsin@gmail.com>
2025-02-12 22:35:39 -0800

    renamed method in CommonPage removed clickWorkSpaceBtn methods from HomePage and EditPage

--- src/test/java/capstoneframework/capstonetests/CapstoneTests.java
+++ src/test/java/capstoneframework/capstonetests/CapstoneTests.java
@@ -1,6 +1,11 @@
 package capstoneframework.capstonetests;
 
 import capstoneframework.basetest.BaseTests;
+import org.testng.annotations.Test;
 
 public class CapstoneTests extends BaseTests {
+
+    @Test
+    public void capstonetest() {
+    }
 }
