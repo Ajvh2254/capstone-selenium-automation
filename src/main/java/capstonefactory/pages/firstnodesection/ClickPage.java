335e309d259d4838bbb000447a7d846214800c64
anthonyhenderson <cuzixsin@gmail.com>
2025-02-19 23:54:38 -0800

    updated clickPage tasks

--- src/main/java/capstonefactory/pages/firstnodesection/ClickPage.java
+++ src/main/java/capstonefactory/pages/firstnodesection/ClickPage.java
@@ -2,6 +2,9 @@ package capstonefactory.pages.firstnodesection;
 
 import capstonefactory.base.BasePage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.interactions.Actions;
 
 public class ClickPage extends BasePage {
 
@@ -14,9 +17,26 @@ public class ClickPage extends BasePage {
         return this;
     }
 
-    public ClickPage clickGoToHomeBtn() {
-        click.waitAndClick(By.cssSelector("#home.button.is-link"));
+    public ClickPage getCoordinates() {
+        Point location = driver.findElement(By.id("position")).getLocation();
+        System.out.println("Location of X: " + location.getX() + "," + "Location of Y: " + location.getY());
         return this;
-    } // X: 166, Y: 416
+    }
+
+    public ClickPage getSize() {
+        Dimension size = driver.findElement(By.cssSelector("#property.button.is-success")).getSize();
+        System.out.println("Width of button: " + size.getWidth() + "," + "Height of button: " + size.getHeight());
+        return this;
+    }
+
+    public ClickPage isBtnDisabled() {
+        isEnabled.isDisabled(By.xpath("//button[@title='Disabled button']"));
+        return this;
+    }
+
+    public void clickAndHoldBtn() {
+        Actions actions = new Actions(driver);
+        actions.clickAndHold(driver.findElement(By.xpath("//h2[contains(text(), ' Button Hold!')]"))).build().perform();
+    }
 
 }
