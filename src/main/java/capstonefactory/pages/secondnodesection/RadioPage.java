2475e04861881efd1ee417b21d2ff6dd22aadcdd
anthonyhenderson <cuzixsin@gmail.com>
2025-02-20 20:35:57 -0800

    new secondNodeSection package

---
+++ src/main/java/capstonefactory/pages/secondnodesection/RadioPage.java
@@ -0,0 +1,11 @@
+package capstonefactory.pages.secondnodesection;
+
+import capstonefactory.base.BasePage;
+
+public class RadioPage extends BasePage {
+
+    public RadioPage() {
+        super();
+    }
+
+}
---
+++ src/main/java/capstonefactory/pages/secondnodesection/WindowPage.java
@@ -0,0 +1,11 @@
+package capstonefactory.pages.secondnodesection;
+
+import capstonefactory.base.BasePage;
+
+public class WindowPage extends BasePage {
+
+    public WindowPage() {
+        super();
+    }
+
+}
