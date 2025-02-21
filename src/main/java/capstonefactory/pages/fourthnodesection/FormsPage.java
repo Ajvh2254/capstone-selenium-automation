e6b3b256249a18313fa3a5ff528938bec8d6e20c
anthonyhenderson <cuzixsin@gmail.com>
2025-02-20 20:41:02 -0800

    new fourthNodeSection package

---
+++ src/main/java/capstonefactory/pages/fourthnodesection/FormsPage.java
@@ -0,0 +1,11 @@
+package capstonefactory.pages.fourthnodesection;
+
+import capstonefactory.base.BasePage;
+
+public class FormsPage extends BasePage {
+
+    public FormsPage() {
+        super();
+    }
+
+}
