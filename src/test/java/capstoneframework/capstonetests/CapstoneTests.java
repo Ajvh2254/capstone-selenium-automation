fa369212edc03ecfe1c756363ccf5989a7e659ba
anthonyhenderson <cuzixsin@gmail.com>
2025-02-20 20:28:37 -0800

    initialized homepage in capstoneTests

--- src/test/java/capstoneframework/capstonetests/CapstoneTests.java
+++ src/test/java/capstoneframework/capstonetests/CapstoneTests.java
@@ -1,5 +1,7 @@
 package capstoneframework.capstonetests;
 
+import capstonefactory.pages.HomePage;
+import capstonefactory.pages.enums.NodeMenuEnums;
 import capstoneframework.basetest.BaseTests;
 import org.testng.annotations.Test;
 
@@ -7,5 +9,8 @@ public class CapstoneTests extends BaseTests {
 
     @Test
     public void capstoneTest() {
+        HomePage homePage = new HomePage();
+        homePage.openLetCodePage()
+                .selectTestSection(NodeMenuEnums.EDIT);
     }
 }
